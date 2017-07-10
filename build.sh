7799394533286cc49d4be61c48402537976ba716
anatoly techtonik <techtonik@gmail.com>
2017-07-10 12:19:56 +0300

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,6 @@
+TARGET=_site
+
+svn co http://svn.code.sf.net/p/farmanager/code/trunk/enc enc
+
+cd $TARGET
+lua ../enc/tools/lua/scripts/tp2hh.lua ../enc/enc_lua/luafar_manual.tsi tsi ../enc/tools/lua/templates/api.tem
